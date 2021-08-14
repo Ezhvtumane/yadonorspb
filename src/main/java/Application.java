07f203a7d0c54d01020f2854d2a8cbd3496da132
Goshan <gogaopel@gmail.com>
2021-08-14 14:10:14 +0300

    found out how parse bloods, start parsing + ideas

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,28 +7,41 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://yadonorspb.ru/svetofor/").get();
-        final Elements elements = doc.select("th");
-        //doc.getElementsByClass("spk-lights__item").first().getElementsByClass("spk-lights__group-item spk-lights__group-item--min").first().attributes()
-        //spk-lights__group-item--min min - mean red,
-        //spk-lights__group-item--middle middle - mean yellow,
-        //spk-lights__group-item--middle max - mean green,
-        //WTF!!??
+        Document doc = Jsoup.connect("https://yadonor.ru/reserve/yadonorspb").get();
+        final Elements elements = doc.getElementsByClass("spk-lights__item");
+        //think about one method to get Blood from page, not map it by pieces
+        // find all spk-lights__group-item--min, spk-lights__group-item--middle, spk-lights__group-item--max 
+        // and than find out what bloodType is it and Rh is it.
+        final List<Element> bloodMin = elements.stream()
+                                        .map(el -> el.getElementsByClass("spk-lights__group-item--min"))
+                                        .flatMap(els -> els.stream())
+                                        .collect(Collectors.toList());
+//find out parent and spk-lights__head inside it -> blood Type ->bloodMin.get(0).parent().parent().getElementsByClass("spk-lights__head").text()
+//find out text in spk-lights__group-item--$ -> Rh ->  bloodMin.get(0).text()               
+
+        final List<Element> bloodMiddle = elements.stream()
+                                        .map(el -> el.getElementsByClass("spk-lights__group-item--middle"))
+                                        .flatMap(els -> els.stream())
+                                        .collect(Collectors.toList());
+        final List<Element> bloodMax = elements.stream()
+                                        .map(el -> el.getElementsByClass("spk-lights__group-item--max"))
+                                        .flatMap(els -> els.stream())
+                                        .collect(Collectors.toList());
+
         final List<String> bloodGroups = parsePageForBloodTypes(elements);
 
         if (bloodGroups.size() > 4) {
             throw new RuntimeException("Too much blood types was parsed!");
         }
 
-        final List<String> bloodNeedsWithRh = parsePageForBloodNeedsWithRh(elements);
+        final List<String> bloodsRh = parsePageForBloodsRh(elements);
 
-        if (bloodNeedsWithRh.size() > 8) {
+        if (bloodsRh.size() > 8) {
             throw new RuntimeException("Too much needs was parsed!");
         }
 
@@ -42,7 +55,7 @@ public class Application {
                 // second part is for RhNegative
                 // that's why we execute 2 times this piece of code
                 // first time for RhPositive and second time for RhNegative
-                final String needsWithRhForAType = bloodNeedsWithRh.remove(0);
+                final String needsWithRhForAType = bloodsRh.remove(0);
                 final BloodType bloodType = convertStringToBloodType(bl);
                 final BloodRh bloodRh = convertStringToBloodRh(needsWithRhForAType);
                 final BloodNeeds bloodNeeds = convertStringToBloodNeeds(needsWithRhForAType);
@@ -60,19 +73,27 @@ public class Application {
 
     static List<String> parsePageForBloodTypes(Elements elements) {
         return elements.stream()
-                .map(el -> el.child(0))//only one child should be
-                .filter(el -> el.tag() == Tag.valueOf("h1"))
-                .map(Element::text)
+                .map(el -> el.getElementsByClass("spk-lights__head"))
+                .flatMap(els -> els.stream())
+                .map(el -> el.text())
                 .collect(Collectors.toList());
+    }
 
+    static List<String> parsePageForBloodsRh(Elements elements) {
+        return elements.stream()
+                .map(el -> el.getElementsByClass("spk-lights__group"))
+                .flatMap(els -> els.stream())
+                .map(el -> el.getElementsByClass("spk-lights__group-item"))
+                .flatMap(els -> els.stream())
+                .map(el -> el.text())
+                .collect(Collectors.toList());
+                //receive 4 pairs of Rh+ Rh- values
     }
 
     static List<String> parsePageForBloodNeedsWithRh(Elements elements) {
-        return elements.stream()
-                .map(el -> el.child(0))//only one child should be
-                .filter(el -> el.tag() == Tag.valueOf("img"))
-                .map(el -> el.attr("alt"))
-                .collect(Collectors.toList());
+        return elements.stream().map(el -> el.child(0))// only one child should be
+                .filter(el -> el.tag() == Tag.valueOf("img")).map(el -> el.attr("alt")).collect(Collectors.toList());
+
     }
 
     static BloodType convertStringToBloodType(String blood) {
